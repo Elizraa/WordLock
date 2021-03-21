12d4e7ef6bd33be644316876459b57fc55b745f2
IzzaArya297 <75309779+IzzaArya297@users.noreply.github.com>
2021-03-21 21:47:09 +0700

    Kunti
    asd

---
+++ WordLockUnity/Assets/Script/Boss/Level2/CollideCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideCheck : MonoBehaviour
+{
+
+    public Aura aura;
+    Vector3 lastVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aura = GetComponentInParent<Aura>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = aura.rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var speed = lastVelocity.magnitude;
+        Vector3 wallNormal = collision.contacts[0].normal;
+        aura.direction = Vector3.Reflect(lastVelocity.normalized, wallNormal);
+        aura.rb.velocity = aura.direction * Mathf.Max(speed, 0f);
+        
+    }
+}
---
+++ WordLockUnity/Assets/Script/Boss/Level2/DeathFromBelow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFromBelow : MonoBehaviour
+{
+    public float indicateTime = 1, damageWeapon, lifeDuration, fade;
+
+    public SpriteRenderer sp;
+    public Collider2D colCheck;
+    public GameObject trueSprite;
+
+
+    float timeSpent = 0;
+    bool damaging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FadeOut", sp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        timeSpent += Time.deltaTime;
+        if(timeSpent >= indicateTime && colCheck.enabled == false)
+        {
+            colCheck.enabled = true;
+            trueSprite.SetActive(true);
+        }
+        if(timeSpent >= lifeDuration)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !damaging)
+        {
+            collision.GetComponent<PlayerControl>().TakeDamage(damageWeapon);
+            damaging = true;
+            
+        }
+    }
+
+    IEnumerator FadeOut(SpriteRenderer spr)
+    {
+        for(float f = 1f; f >= -0.2f; f -= 0.05f)
+        {
+            Color c = spr.material.color;
+            c.a = f;
+            sp.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+}
