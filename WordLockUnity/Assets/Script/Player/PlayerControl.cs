912134d4b05cc3f8b0e9a6581adc291b4203f3bd
IzzaArya297 <75309779+IzzaArya297@users.noreply.github.com>
2021-03-22 03:51:33 +0700

    keris terbang

--- WordLockUnity/Assets/Script/Player/PlayerControl.cs
+++ WordLockUnity/Assets/Script/Player/PlayerControl.cs
@@ -16,6 +16,10 @@ public class PlayerControl : MonoBehaviour
 
     public GameObject boss, keris;
 
+    public Vector2 kerisReady;
+
+    Vector2 kerisOffset;
+
     public float damageFlyingKeris, damageFire, damageKomet;
 
     // Start is called before the first frame update
@@ -25,12 +29,17 @@ public class PlayerControl : MonoBehaviour
         PlayerUI.playerUI.SetMaxMana(manaPlayer);
         manaLeft = manaPlayer;
         maxHealth = health;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FlyingKeris();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             FlyingKeris();
         }
@@ -61,8 +70,51 @@ public class PlayerControl : MonoBehaviour
 
     void FlyingKeris()
     {
+        StartCoroutine("KerisFly");
+        
+    }
 
+    IEnumerator KerisFly()
+    {
+        Vector2 hitLand = boss.transform.position; kerisOffset = keris.transform.position;
+        Rigidbody2D rb = keris.GetComponent<Rigidbody2D>();
+        float time = 0, currentRot = 0;
+
+        while (time < 1f)
+        {
+            keris.transform.position = Vector2.Lerp(kerisOffset, kerisOffset + kerisReady, time / 1f);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        float rotWeapon = Mathf.Atan2(hitLand.y - keris.transform.position.y, hitLand.x - keris.transform.position.x) * Mathf.Rad2Deg - 90;
+
+        time = 0;
+        while (time < 0.5f)
+        {
+           
+            if(time < 0.3)
+            {
+                keris.transform.position = Vector2.Lerp(kerisOffset + kerisReady, hitLand, time / 0.3f);
+                currentRot = Mathf.Lerp(0, rotWeapon, time / 0.3f);
+                rb.rotation = currentRot;
+            }
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        rotWeapon = Mathf.Atan2(keris.transform.position.y - hitLand.y, keris.transform.position.x - hitLand.x) * Mathf.Rad2Deg - 90;
         AttackBoss(Random.Range(damageFlyingKeris, damageFlyingKeris + 100));
+        time = 0;
+
+        while (time < 0.3f)
+        {
+            keris.transform.position = Vector2.Lerp(hitLand, kerisOffset, time / 0.3f);
+            currentRot = Mathf.Lerp(rotWeapon, 0, time / 0.3f);
+            rb.rotation = currentRot;
+
+            time += Time.deltaTime;
+            yield return null;
+        }
     }
 
     void TouchOfFire()
